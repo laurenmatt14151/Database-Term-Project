42cf866b881c916929ebe53b130f5062b1dde1a3
Lauren Matthews <126510096+laurenmatt14151@users.noreply.github.com>
2023-11-29 16:03:56 -0500

    Update petAdoption.sql

--- petAdoption.sql
+++ petAdoption.sql
@@ -26,7 +26,7 @@ CREATE TABLE Dog_Profile(
     DogName TEXT,
     Age INT,
     Size INT,
-    AvailableForAdoption TINYINT(1),
+    AvailableForAdoption TEXT NOT NULL,
     Dog_Description TEXT,
     PRIMARY KEY (DogID),
     FOREIGN KEY (BreedID) REFERENCES Breed_Info(BreedID)
@@ -42,3 +42,29 @@ CREATE TABLE Adoption_Record(
     FOREIGN KEY (DogID) REFERENCES Dog_Profile(DogID)
 );
 
+INSERT INTO Customer_Profile (FirstName, LastName, PhoneNumber, Email, Cust_Address) VALUES 
+('Jacob', 'Johnson', '(904)635-2476', 'jacob.johnson@gmail.com', '2654 Serail Dr, Tampa, FL'),
+('Jane', 'Smith', '(967)654-3210', 'jane.smith@yahoo.com', '456 Oak St, Townburg, AL'),
+('Bob', 'White', '(555)789-4561', 'bob.white@yahoo.com', '123 Main St, Houston, TX'),
+('David', 'Brown', '(573)489-1045', 'david.brown@aol.com', '493 Rosey Dr, Lafayette, LA'),
+('Lindsey', 'Harth', '(395)204-1894', 'lindsey.harth@gmail.com', '205 Pine St, Hamlet, TN');
+
+INSERT INTO Breed_Info (BreedName, HairType, DiseasesProneTo) VALUES 
+('Labrador', 'Short', 'Hip Dysplasia'),
+('Poodle', 'Curly', 'Progressive Retinal Atrophy'),
+('German Shepherd', 'Medium', 'Elbow Dysplasia'),
+('Beagle', 'Short', 'Epilepsy'),
+('Bulldog', 'Short', 'Respiratory Problems');
+
+INSERT INTO Dog_Profile (DogName, Age, Size, AvailableForAdoption, Dog_Description) VALUES 
+('Buddy', 2, 'Medium', 'Yes', 'Very friendly and affectionate'),
+('Rocky', 3, 'Large', 'Yes', 'Gorgeous and energetic dog'),
+('Daisy', 1, 'Small', 'No', 'The sweetest dog with a bubbly personality'),
+('Max', 4, 'Small', 'Yes', 'Happy dog with a warm personality'),
+('Bella', 2, 'Medium', 'No', 'Calm and quiet dog that will bring peace to your life');
+
+INSERT INTO Adoption_Record (CustomerID, DogID, AdoptionDate) VALUES 
+(1, 3, '2022-01-15'),
+(3, 5, '2022-08-02'),
+(2, 3, '2023-05-18'),
+(4, 5, '2023-09-20');
